93c83fbb8376bb77ccdaca058f20c85fd4017880
tomharrigan <tom29axp@gmail.com>
2014-12-09 19:56:58 -0500

    Add Genesis Support
    adds genesis.php file to theme
    
    Consists of adding theme support, defining container div and render function. Render function is essentially the genesis_standard_loop() function that's located in the /lib/structure/loops.php file
    
    Todo: Do something better with the footer than defining it as false

---
+++ themes/genesis.php
@@ -0,0 +1,58 @@
+<?php
+/**
+ * Infinite Scroll Theme Assets
+ *
+ * Register support for Genesis.
+ */
+
+/**
+ * Add theme support for infinite scroll
+ */
+function genesis_infinite_transporter_init() {
+    add_theme_support( 'infinite-transporter', array(
+        'container' => 'main-content',
+        'footer'    => false,
+        'render'    => 'genesis_infinite_transporter_render'
+    ) );
+}
+add_action( 'after_setup_theme', 'genesis_infinite_transporter_init' );
+
+/**
+ * Set the code to be rendered on for calling posts,
+ * hooked to template parts when possible.
+ */
+function genesis_infinite_transporter_render() {
+
+        if ( have_posts() ) :
+
+            do_action( 'genesis_before_while' );
+            while ( have_posts() ) : the_post();
+
+                do_action( 'genesis_before_entry' );
+
+                printf( '<article %s>', genesis_attr( 'entry' ) );
+
+                    do_action( 'genesis_entry_header' );
+
+                    do_action( 'genesis_before_entry_content' );
+
+                    printf( '<div %s>', genesis_attr( 'entry-content' ) );
+                    do_action( 'genesis_entry_content' );
+                    echo '</div>';
+
+                    do_action( 'genesis_after_entry_content' );
+
+                    do_action( 'genesis_entry_footer' );
+
+                echo '</article>';
+
+                do_action( 'genesis_after_entry' );
+
+            endwhile; //* end of one post
+
+        else : //* if no posts exist
+            do_action( 'genesis_loop_else' );
+        endif; //* end loop
+        
+}
+add_action( 'init', 'metro_pro_infinite_transporter_init' );
