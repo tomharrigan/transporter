5acece83ae2303ceec1a7e3a5a9de6abf7c61ba8
tomharrigan <tom29axp@gmail.com>
2014-12-10 17:09:17 -0500

    Add Canvas theme support

---
+++ themes/canvas.php
@@ -0,0 +1,39 @@
+<?php
+/**
+ * Infinite Scroll Theme Assets
+ *
+ * Register support for Genesis.
+ */
+
+/**
+ * Add theme support for infinite scroll
+ */
+function canvas_infinite_transporter_init() {
+    add_theme_support( 'infinite-transporter', array(
+        'container' => 'main',
+        'render'    => 'canvas_infinite_transporter_render'
+    ) );
+}
+add_action( 'after_setup_theme', 'canvas_infinite_transporter_init' );
+
+/**
+ * Set the code to be rendered on for calling posts,
+ * hooked to template parts when possible.
+ */
+function canvas_infinite_transporter_render() {
+
+    if( is_single() ) {
+
+        woo_loop_before();
+        
+        if (have_posts()) { $count = 0;
+            while (have_posts()) { the_post(); $count++;
+                
+                woo_get_template_part( 'content', get_post_type() ); // Get the post content template file, contextually.
+            }
+        }
+        
+        woo_loop_after();
+    }
+        
+}
