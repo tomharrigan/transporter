f2fcab5fc3b9bffedeee7980d56bcccdeb224bee
tomharrigan <tom29axp@gmail.com>
2015-01-12 00:59:18 -0500

    fix twentyfifteen theme style load

--- themes/twentyfifteen.php
+++ themes/twentyfifteen.php
@@ -20,7 +20,7 @@ add_action( 'after_setup_theme', 'twentyfifteen_infinite_transporter_init' );
  * Enqueue CSS stylesheet with theme styles for Infinite Transporter.
  */
 function twentyfifteen_infinite_transporter_enqueue_styles() {
-	wp_enqueue_style( 'infinity-twentyfifteen', plugins_url( 'twentyfifteen.css', __FILE__ ), array( 'the-neverending-homepage' ), '20141022' );
+	wp_enqueue_style( 'infinity-twentyfifteen', plugins_url( 'twentyfifteen.css', __FILE__ ), array( 'mcninja-post-transporter' ), '20141022' );
 	wp_style_add_data( 'infinity-twentyfifteen', 'rtl', 'replace' );
 }
 add_action( 'wp_enqueue_scripts', 'twentyfifteen_infinite_transporter_enqueue_styles', 25 );
